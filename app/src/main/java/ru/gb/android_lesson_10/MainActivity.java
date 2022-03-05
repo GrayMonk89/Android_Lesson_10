5ca10c06d603093f9379eacb63fca6ae1f3bb48d
Gray Monk <graymonk89@yandex.ru>
2022-03-05 10:45:30 +0300

    10 урок. Косметика. Добавил тулбар и фрагмент новой заметки

--- app/src/main/java/ru/gb/android_lesson_10/MainActivity.java
+++ app/src/main/java/ru/gb/android_lesson_10/MainActivity.java
@@ -1,9 +1,15 @@
 package ru.gb.android_lesson_10;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import ru.gb.android_lesson_10.ui.NewNoteFragment;
 import ru.gb.android_lesson_10.ui.NoteListFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +22,33 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.notesContainer, NoteListFragment.newInstance()).commit();
         }
+        setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.action_new_note):{
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.notesContainer, NewNoteFragment.newInstance()).addToBackStack("").commit();
+//                if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+//                    getSupportFragmentManager().beginTransaction().replace(R.id.the_note, NewNoteFragment.newInstance()).addToBackStack("").commit();
+//                } else {
+//                    getSupportFragmentManager().beginTransaction().replace(R.id.list_of_note, NewNoteFragment.newInstance()).addToBackStack("").commit();
+//                };
+                return true;
+            }
+            case (R.id.action_exit): {
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/ru/gb/android_lesson_10/ui/NewNoteFragment.java
@@ -0,0 +1,79 @@
+package ru.gb.android_lesson_10.ui;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import ru.gb.android_lesson_10.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NewNoteFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewNoteFragment extends Fragment {
+
+    public static NewNoteFragment newInstance() {
+        NewNoteFragment fragment = new NewNoteFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.new_note_menu, menu);
+        menu.findItem(R.id.action_new_note).setVisible(false);
+        menu.findItem(R.id.action_exit).setVisible(false);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_note, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setHasOptionsMenu(true);
+        initView(view);
+    }
+
+    void initView(View view){
+        view.findViewById(R.id.add_new_note).setOnClickListener(v -> {addNote();});
+        view.findViewById(R.id.go_back).setOnClickListener(v ->{goBack();});
+    }
+    private void addNote() {
+
+
+
+        Toast.makeText(requireContext(), "Типо добавил заметку", Toast.LENGTH_SHORT).show();
+        requireActivity().onBackPressed();
+    }
+
+    void goBack() {
+        requireActivity().onBackPressed();
+    }
+
+}
